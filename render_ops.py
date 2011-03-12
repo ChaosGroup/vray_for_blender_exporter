a372415453c0713360b8b54c307c0a2ae0ecd3ce
Andrey M. Izrantsev <izrantsev@gmail.com>
2011-03-12 10:26:06 +0300

    fix: init(context) preview

--- render_ops.py
+++ render_ops.py
@@ -4,7 +4,7 @@
 
   http://vray.cgdo.ru
 
-  Time-stamp: "Saturday, 12 March 2011 [09:48]"
+  Time-stamp: "Saturday, 12 March 2011 [10:25]"
 
   Author: Andrey M. Izrantsev (aka bdancer)
   E-Mail: izrantsev@cgdo.ru
@@ -614,7 +614,7 @@ def init(context):
 	bus['scene']= scene
 
 	# Preview
-	bus['preview']= preview
+	bus['preview']= False
 	
 	# V-Ray uses UV indexes, Blender uses UV names
 	# Here we store UV name->index map
