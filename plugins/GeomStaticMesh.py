777cb7f313bb0b32d85d9d244ad08f47c8ad0fc4
Andrey M. Izrantsev <izrantsev@gmail.com>
2011-03-19 13:47:27 +0300

    add: hex lists format

--- plugins/GeomStaticMesh.py
+++ plugins/GeomStaticMesh.py
@@ -3,7 +3,7 @@
 
   http://vray.cgdo.ru
 
-  Time-stamp: "Monday, 14 March 2011 [14:44]"
+  Time-stamp: "Saturday, 19 March 2011 [13:42]"
 
   Author: Andrey M. Izrantsev (aka bdancer)
   E-Mail: izrantsev@cgdo.ru
@@ -24,10 +24,6 @@
   All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Software.
 '''
 
-
-''' Python modules '''
-import struct
-import socket
 
 ''' Blender modules '''
 import bpy
@@ -58,6 +54,141 @@ def add_properties(rna_pointer):
 		type=  GeomStaticMesh,
 		description= "V-Ray static mesh settings."
 	)
+
+
+def write_mesh_hex(bus):
+	ofile=   bus['files']['geometry'][0]
+	scene=   bus['scene']
+	ob=      bus['node']['object']
+	me=      bus['node']['mesh']
+	me_name= bus['node']['mesh_name']
+
+	face_tri= (0,1,2,2,3,0)
+	
+	ofile.write("\nGeomStaticMesh %s {" % me_name)
+
+	ofile.write("\n\tvertices= interpolate((%d, ListVectorHex(\""%(scene.frame_current))
+	for v in me.vertices:
+		for c in v.co:
+			ofile.write(HexFormat(c))
+	ofile.write("\")));")
+
+	ofile.write("\n\tfaces= interpolate((%d, ListIntHex(\""%(scene.frame_current))
+	for f in me.faces:
+		if len(f.vertices) == 4:
+			for i in face_tri:
+				ofile.write(HexFormat(f.vertices[i]))
+		else:
+			for v in f.vertices:
+				ofile.write(HexFormat(v))
+	ofile.write("\")));")
+
+	ofile.write("\n\tface_mtlIDs= ListIntHex(\"")
+	for f in me.faces:
+		if len(f.vertices) == 4:
+			ofile.write(HexFormat(f.material_index + 1))
+			ofile.write(HexFormat(f.material_index + 1))
+		else:
+			ofile.write(HexFormat(f.material_index + 1))
+	ofile.write("\");")
+
+	ofile.write("\n\tnormals= interpolate((%d, ListVectorHex(\""%(scene.frame_current))
+	for f in me.faces:
+		if len(f.vertices) == 4:
+			vertices= face_tri
+		else:
+			vertices= (0,1,2)
+
+		for v in vertices:
+			if f.use_smooth:
+				for c in me.vertices[f.vertices[v]].normal:
+					ofile.write(HexFormat(c))
+			else:
+				for c in f.normal:
+					ofile.write(HexFormat(c))
+	ofile.write("\")));")
+
+	ofile.write("\n\tfaceNormals= ListIntHex(\"")
+	k= 0
+	for f in me.faces:
+		if len(f.vertices) == 4:
+			vertices= 6
+		else:
+			vertices= 3
+
+		for v in range(vertices):
+			ofile.write(HexFormat(k))
+			k+= 1
+	ofile.write("\");")
+
+
+	def edge_visibility(k, ev):
+		if k == 9:
+			ofile.write(HexFormat(int(ev, 2)))
+			return 0, ""
+		return k + 1, ev
+
+	ofile.write("\n\tedge_visibility= ListIntHex(\"")
+
+	k= 0
+	ev= ""
+	if len(me.faces) < 5:
+		for f in me.faces:
+			if len(f.vertices) == 4:
+				ev+= "011011"
+			else:
+				ev+= "111"
+		edge_visibility(k, ev)
+	else:
+		k= 0;
+		for f in me.faces:
+			if len(f.vertices) == 4:
+				ev+= "011"
+				k, ev = edge_visibility(k, ev)
+				ev+= "011"
+				k, ev = edge_visibility(k, ev)
+			else:
+				ev+= "111"
+				k, ev = edge_visibility(k, ev)
+		if k:
+			edge_visibility(k, ev)
+	ofile.write("\");")
+
+
+	if len(me.uv_textures):
+		ofile.write("\n\tmap_channels= List(")
+
+		for uv_texture_idx,uv_texture in enumerate(me.uv_textures):
+			if uv_texture_idx:
+				ofile.write(",")
+
+			uv_layer_index= get_uv_layer_id(bus['uvs'], uv_texture.name)
+
+			ofile.write("\n\t\t// %s"%(uv_texture.name))
+			ofile.write("\n\t\tList(%d,ListVectorHex(\""%(uv_layer_index))
+
+			for face in uv_texture.data:
+				for uv in face.uv:
+					ofile.write(HexFormat(uv[0]))
+					ofile.write(HexFormat(uv[1]))
+					ofile.write(HexFormat(0.0))
+
+			ofile.write("\"),ListIntHex(\"")
+
+			k= 0
+			for face in uv_texture.data:
+				if len(face.uv) == 4:
+					for i in face_tri:
+						ofile.write(HexFormat(k+i))
+					k+= 4
+				else:
+					for i in (0,1,2):
+						ofile.write(HexFormat(k+i))
+					k+= 3
+			ofile.write("\"))")
+
+		ofile.write(");")
+	ofile.write("\n}\n")
 
 
 def write_mesh_ascii(bus):
@@ -237,10 +368,7 @@ def write(bus):
 		  "Frame {0}: Mesh: \033[0;32m{1:<32}\033[0m".format(scene.frame_current, color(ob.data.name, 'green')),
 		  newline= VRayExporter.debug)
 
-	# TODO
-	# if VRayExporter.mesh_ascii:
-	# 	write_mesh_ascii(bus)
-	# else:
-	# 	write_mesh_hex(bus)
-	write_mesh_ascii(bus)
-
+	if VRayExporter.mesh_ascii:
+		write_mesh_ascii(bus)
+	else:
+		write_mesh_hex(bus)
