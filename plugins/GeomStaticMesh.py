b27ef163f1a0662f07ef9f1f819a82707cbe09cd
Andrey M. Izrantsev <izrantsev@gmail.com>
2012-04-27 16:10:32 +0400

    fix: api update; ascii removed

--- plugins/GeomStaticMesh.py
+++ plugins/GeomStaticMesh.py
@@ -69,6 +69,8 @@ def write_mesh_hex(bus):
 	me_name= bus['node']['mesh_name']
 
 	GeomStaticMesh= ob.data.vray.GeomStaticMesh
+	
+	face_attr = 'faces' if 'faces' in dir(me) else 'polygons'
 
 	face_tri= (0,1,2,2,3,0)
 	
@@ -81,7 +83,7 @@ def write_mesh_hex(bus):
 	ofile.write("\")));")
 
 	ofile.write("\n\tfaces= interpolate((%d, ListIntHex(\""%(scene.frame_current))
-	for f in me.faces:
+	for f in getattr(me, face_attr):
 		if len(f.vertices) == 4:
 			for i in face_tri:
 				ofile.write(HexFormat(f.vertices[i]))
@@ -91,7 +93,7 @@ def write_mesh_hex(bus):
 	ofile.write("\")));")
 
 	ofile.write("\n\tface_mtlIDs= ListIntHex(\"")
-	for f in me.faces:
+	for f in getattr(me, face_attr):
 		if len(f.vertices) == 4:
 			ofile.write(HexFormat(f.material_index + 1))
 			ofile.write(HexFormat(f.material_index + 1))
@@ -100,7 +102,7 @@ def write_mesh_hex(bus):
 	ofile.write("\");")
 
 	ofile.write("\n\tnormals= interpolate((%d, ListVectorHex(\""%(scene.frame_current))
-	for f in me.faces:
+	for f in getattr(me, face_attr):
 		if len(f.vertices) == 4:
 			vertices= face_tri
 		else:
@@ -117,7 +119,7 @@ def write_mesh_hex(bus):
 
 	ofile.write("\n\tfaceNormals= ListIntHex(\"")
 	k= 0
-	for f in me.faces:
+	for f in getattr(me, face_attr):
 		if len(f.vertices) == 4:
 			vertices= 6
 		else:
@@ -139,8 +141,8 @@ def write_mesh_hex(bus):
 
 	k= 0
 	ev= ""
-	if len(me.faces) < 5:
-		for f in me.faces:
+	if len(getattr(me, face_attr)) < 5:
+		for f in getattr(me, face_attr):
 			if len(f.vertices) == 4:
 				ev+= "011011"
 			else:
@@ -148,7 +150,7 @@ def write_mesh_hex(bus):
 		edge_visibility(k, ev)
 	else:
 		k= 0;
-		for f in me.faces:
+		for f in getattr(me, face_attr):
 			if len(f.vertices) == 4:
 				ev+= "011"
 				k, ev = edge_visibility(k, ev)
@@ -204,172 +206,6 @@ def write_mesh_hex(bus):
 	ofile.write("\n}\n")
 
 
-def write_mesh_ascii(bus):
-	ofile=   bus['files']['geometry'][0]
-	scene=   bus['scene']
-	ob=      bus['node']['object']
-	me=      bus['node']['mesh']
-	me_name= bus['node']['mesh_name']
-	
-	ofile.write("\nGeomStaticMesh %s {" % me_name)
-
-	ofile.write("\n\tvertices= interpolate((%d, ListVector("%(scene.frame_current))
-	for v in me.vertices:
-		if(v.index):
-			ofile.write(",")
-		ofile.write("Vector(%.6f,%.6f,%.6f)"%(tuple(v.co)))
-	ofile.write(")));")
-
-	ofile.write("\n\tfaces= interpolate((%d, ListInt("%(scene.frame_current))
-	for f in me.faces:
-		if f.index:
-			ofile.write(",")
-		if len(f.vertices) == 4:
-			ofile.write("%d,%d,%d,%d,%d,%d"%(
-				f.vertices[0], f.vertices[1], f.vertices[2],
-				f.vertices[2], f.vertices[3], f.vertices[0]))
-		else:
-			ofile.write("%d,%d,%d"%(
-				f.vertices[0], f.vertices[1], f.vertices[2]))
-	ofile.write(")));")
-
-	ofile.write("\n\tface_mtlIDs= ListInt(")
-	for f in me.faces:
-		if f.index:
-			ofile.write(",")
-		if len(f.vertices) == 4:
-			ofile.write("%d,%d"%(
-				f.material_index + 1, f.material_index + 1))
-		else:
-			ofile.write("%d"%(
-				f.material_index + 1))
-	ofile.write(");")
-
-	ofile.write("\n\tnormals= interpolate((%d, ListVector("%(scene.frame_current))
-	for f in me.faces:
-		if f.index:
-			ofile.write(",")
-
-		if len(f.vertices) == 4:
-			vertices= (0,1,2,2,3,0)
-		else:
-			vertices= (0,1,2)
-
-		comma= 0
-		for v in vertices:
-			if comma:
-				ofile.write(",")
-			comma= 1
-
-			if f.use_smooth:
-				ofile.write("Vector(%.6f,%.6f,%.6f)"%(
-					tuple(me.vertices[f.vertices[v]].normal)
-				))
-			else:
-				ofile.write("Vector(%.6f,%.6f,%.6f)"%(
-					tuple(f.normal)
-				))
-	ofile.write(")));")
-
-	ofile.write("\n\tfaceNormals= ListInt(")
-	k= 0
-	for f in me.faces:
-		if f.index:
-			ofile.write(",")
-
-		if len(f.vertices) == 4:
-			vertices= 6
-		else:
-			vertices= 3
-
-		for v in range(vertices):
-			if v:
-				ofile.write(",")
-			ofile.write("%d"%(k))
-			k+= 1
-	ofile.write(");")
-
-
-	def edge_visibility(k, ev):
-		if k == 9:
-			ofile.write("%i," % int(ev, 2))
-			return 0, ""
-		return k + 1, ev
-
-	ofile.write("\n\tedge_visibility= ListInt(")
-
-	k= 0
-	ev= ""
-	if len(me.faces) < 5:
-		for f in me.faces:
-			if len(f.vertices) == 4:
-				ev+= "011011"
-			else:
-				ev+= "111"
-		edge_visibility(k, ev)
-	else:
-		k= 0;
-		for f in me.faces:
-			if len(f.vertices) == 4:
-				ev+= "011"
-				k, ev = edge_visibility(k, ev)
-				ev+= "011"
-				k, ev = edge_visibility(k, ev)
-			else:
-				ev+= "111"
-				k, ev = edge_visibility(k, ev)
-		if k:
-			edge_visibility(k, ev)
-	ofile.write("0);")
-
-
-	if len(me.uv_textures):
-		ofile.write("\n\tmap_channels= List(")
-
-		for uv_texture_idx,uv_texture in enumerate(me.uv_textures):
-			if uv_texture_idx:
-				ofile.write(",")
-
-			uv_layer_index= get_uv_layer_id(bus['uvs'], uv_texture.name)
-
-			ofile.write("\n\t\t// %s"%(uv_texture.name))
-			ofile.write("\n\t\tList(%d,ListVector("%(uv_layer_index))
-
-			for f in range(len(uv_texture.data)):
-				if f:
-					ofile.write(",")
-
-				face= uv_texture.data[f]
-
-				for i in range(len(face.uv)):
-					if i:
-						ofile.write(",")
-					ofile.write("Vector(%.6f,%.6f,0.0)"%(
-						face.uv[i][0],
-						face.uv[i][1]
-					))
-
-			ofile.write("),ListInt(")
-
-			k= 0
-			for f in range(len(uv_texture.data)):
-				if f:
-					ofile.write(",")
-
-				face= uv_texture.data[f]
-
-				if len(face.uv) == 4:
-					ofile.write("%i,%i,%i,%i,%i,%i" % (k,k+1,k+2,k+2,k+3,k))
-					k+= 4
-				else:
-					ofile.write("%i,%i,%i" % (k,k+1,k+2))
-					k+= 3
-			ofile.write("))")
-
-		ofile.write(");")
-	ofile.write("\n}\n")
-
-
 def write(bus):
 	scene= bus['scene']
 	ob=    bus['node']['object']
@@ -381,7 +217,4 @@ def write(bus):
 		  "Frame {0}: Mesh: \033[0;32m{1:<32}\033[0m".format(scene.frame_current, color(ob.data.name, 'green')),
 		  newline= VRayExporter.debug)
 
-	if VRayExporter.mesh_ascii:
-		write_mesh_ascii(bus)
-	else:
-		write_mesh_hex(bus)
+	write_mesh_hex(bus)
