1afca4199f2c81f6b16ac6fd33c806ea230f55fb
Andrey M. Izrantsev <izrantsev@gmail.com>
2013-07-29 20:19:42 +0400

    fix: material preview update when changin preset

--- presets/material/Pure_glass.py
+++ presets/material/Pure_glass.py
@@ -1,24 +1,20 @@
 import bpy
 
-def active_node_material(ma):
-	ma_node= ma.active_node_material
-	if ma_node:
-		return ma_node
-	else:
-		return ma
+from bl_ui.properties_material import active_node_mat
 
-ma= active_node_material(bpy.context.active_object.active_material)
 
-ma.diffuse_color = 0.0,0.0,0.0
+ma = active_node_mat(bpy.context.active_object.active_material)
 
-VRayMaterial= ma.vray
-VRayMaterial.type= 'BRDFVRayMtl'
+VRayMaterial = ma.vray
+VRayMaterial.type = 'BRDFVRayMtl'
+VRayMaterial.Mtl2Sided.use = False
 
-BRDFVRayMtl= VRayMaterial.BRDFVRayMtl
+BRDFVRayMtl = VRayMaterial.BRDFVRayMtl
 
-BRDFVRayMtl.fog_color= 1.0,1.0,1.0
-BRDFVRayMtl.refract_color= 1.0,1.0,1.0
-BRDFVRayMtl.refract_ior= 1.55
-BRDFVRayMtl.refract_affect_shadows= True
-BRDFVRayMtl.reflect_color= 0.95,0.95,0.95
-BRDFVRayMtl.fresnel= True
+BRDFVRayMtl.diffuse = 0.0,0.0,0.0
+BRDFVRayMtl.fog_color = 1.0,1.0,1.0
+BRDFVRayMtl.refract_color = 1.0,1.0,1.0
+BRDFVRayMtl.refract_ior = 1.55
+BRDFVRayMtl.refract_affect_shadows = True
+BRDFVRayMtl.reflect_color = 0.95,0.95,0.95
+BRDFVRayMtl.fresnel = True
