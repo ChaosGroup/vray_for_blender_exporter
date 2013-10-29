93b6a7bea6da4d409995b099304ac2a3eac20cb8
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2013-10-29 17:10:14 +0200

    fix: draw changed to match new call

--- plugins/texture/BitmapBuffer.py
+++ plugins/texture/BitmapBuffer.py
@@ -189,9 +189,7 @@ def nodeDraw(context, layout, node):
     row.operator("vray.open_image", icon='ZOOMIN', text="")
 
 
-def gui(context, layout, node):
-    BitmapBuffer = node.BitmapBuffer
-
+def gui(context, layout, BitmapBuffer, node):
     if node.image:
         layout.prop(node.image, 'name')
         layout.prop(node.image, 'filepath')
