79af77d176a9f4c16c615a9cfb4e94a9758584f4
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2013-11-27 16:03:42 +0200

    fix: formatting

--- plugins/system/Exporter.py
+++ plugins/system/Exporter.py
@@ -90,9 +90,9 @@ class VRayExporter(bpy.types.PropertyGroup):
         name        = "Active layers",
         description = "Render objects from layers",
         items = (
-                ('ACTIVE', "Active", ""),
-                ('ALL',    "All",    ""),
-                ('CUSTOM', "Custom", "")
+            ('ACTIVE', "Active", ""),
+            ('ALL',    "All",    ""),
+            ('CUSTOM', "Custom", "")
         ),
         default = 'ACTIVE'
     )
