add988c5eefb2d5f517d9097f4da8dbd69e7d22d
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2013-12-12 16:14:15 +0200

    fix: render id channel resolved

--- plugins/renderchannel/RenderChannelRenderID.py
+++ plugins/renderchannel/RenderChannelRenderID.py
@@ -1,84 +1,41 @@
-'''
+#
+# V-Ray For Blender
+#
+# http://vray.cgdo.ru
+#
+# Author: Andrei Izrantcev
+# E-Mail: andrei.izrantcev@chaosgroup.com
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+# All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Software.
+#
 
-  V-Ray/Blender
+TYPE = 'RENDERCHANNEL'
+ID   = 'RenderChannelRenderID'
+NAME = 'Render ID'
+DESC = "The object's \"Pass Index\" contributes the pixel value"
 
-  http://vray.cgdo.ru
-
-  Author: Andrey M. Izrantsev (aka bdancer)
-  E-Mail: izrantsev@cgdo.ru
-
-  This program is free software; you can redistribute it and/or
-  modify it under the terms of the GNU General Public License
-  as published by the Free Software Foundation; either version 2
-  of the License, or (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
-  All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Software.
-  
-'''
-
-TYPE= 'RENDERCHANNEL'
-ID=   'RENDERID'
-NAME= 'RenderID'
-PLUG= 'RenderChannelRenderID'
-DESC= ""
-PID=  6
-
-PARAMS= (
-	'name'
+PluginParams = (
+    {
+        'attr' : 'name',
+        'desc' : "Channel name",
+        'type' : 'STRING',
+        'default' : NAME,
+    },
 )
 
 
-''' Blender modules '''
-import bpy
-from bpy.props import *
-
-''' vb modules '''
-from vb25.utils import *
-
-
-def add_properties(parent_struct):
-	class RenderChannelRenderID(bpy.types.PropertyGroup):
-		pass
-	bpy.utils.register_class(RenderChannelRenderID)
-	
-	parent_struct.RenderChannelRenderID= PointerProperty(
-		name= "RenderID",
-		type=  RenderChannelRenderID,
-		description= "V-Ray render channel \"RenderID\" settings"
-	)
-
-	RenderChannelRenderID.name= StringProperty(
-		name= "Name",
-		description= "",
-		default= "RenderID"
-	)
-
-
-
-'''
-  OUTPUT
-'''
-def write(ofile, render_channel, sce= None, name= None):
-	channel_name= render_channel.name
-	if name is not None:
-		channel_name= name
-
-	ofile.write("\n%s %s {" % (PLUG, clean_string(channel_name)))
-	ofile.write("\n\tname= \"%s\";" % clean_string(channel_name))
-	ofile.write("\n}\n")
-
-
-
-'''
-  GUI
-'''
-def draw(rna_pointer, layout, wide_ui):
-	layout.label(text="The object's \"Pass index\" contributes the pixel value")
+def nodeDraw(context, layout, propGroup):
+    layout.prop(propGroup, 'name')
