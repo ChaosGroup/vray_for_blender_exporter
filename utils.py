83a61fcbf31a8552b71d509c7ac2932f596e9996
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-02-11 16:04:45 +0200

    fix: copytree resolved

--- utils.py
+++ utils.py
@@ -451,8 +451,10 @@ def get_username():
 
 def copytree(src, dst, symlinks=False, ignore=None):
 	if PLATFORM == 'win32':
-		os.system("robocopy /E %s %s" % (src, dst))
+		os.system('robocopy /E "%s" "%s"' % (src, dst))
 	else:
+		if not os.path.exists(dst):
+			os.makedirs(dst)
 		for item in os.listdir(src):
 			s = os.path.join(src, item)
 			d = os.path.join(dst, item)
