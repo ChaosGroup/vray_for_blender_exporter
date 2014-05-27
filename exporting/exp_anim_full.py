5880e0a22ab498e93aeb9bb564b57f4c499820ec
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-05-27 14:10:13 +0300

    fix: all modes resolved

--- exporting/exp_anim_full.py
+++ exporting/exp_anim_full.py
@@ -28,37 +28,109 @@ from vb30 import debug
 
 from . import exp_init
 from . import exp_scene
+from . import exp_camera
 
 
-@debug.TimeIt
-def ExportAnimation(bus, frameStart, frameEnd, frameStep):
-    debug.Debug("ExportAnimation()")
-
-    err = None
-
+def ExportCameraOnly(bus):
     scene = bus['scene']
     o     = bus['output']
-
-    VRayScene = scene.vray
-    VRayExporter = VRayScene.Exporter
-
-    o.setAnimation(True)
-
-    o.setFrameStart(frameStart)
-    o.setFrameEnd(frameEnd)
-
-    # Set frame step; used to detect if we need
-    # to export a keyframe in interpolate()
-    o.setFrameStep(frameStep)
-
-    # Store current frame
-    selected_frame = scene.frame_current
 
     # Init exporter
     bus['exporter'] = exp_init.InitExporter(bus, isAnimation=True)
 
-    f = frameStart
-    while(f <= frameEnd):
+    # Store current frame
+    selected_frame = scene.frame_current
+
+    # Set frame
+    f = o.frameStart
+
+    scene.frame_set(f)
+    o.setFrame(f)
+    _vray_for_blender.setFrame(f)
+
+    # 1. Export full first frame
+    err = exp_scene.ExportScene(bus)
+    if err is not None:
+        return err
+
+    # 2. Export camera motion for the rest frames
+    f += o.frameStep
+    while(f <= o.frameEnd):
+        scene.frame_set(f)
+        o.setFrame(f)
+        _vray_for_blender.setFrame(f)
+
+        err = exp_camera.ExportCamera(bus)
+        if err is not None:
+            break
+
+        f += o.frameStep
+
+    exp_init.ShutdownExporter(bus)
+
+    # Restore selected frame
+    scene.frame_set(selected_frame)
+
+    return None
+
+
+def ExportFullNotMeshes(bus):
+    scene = bus['scene']
+    o     = bus['output']
+
+    # Init exporter
+    bus['exporter'] = exp_init.InitExporter(bus, isAnimation=True)
+
+    # Store current frame
+    selected_frame = scene.frame_current
+
+    # Set frame
+    f = o.frameStart
+
+    scene.frame_set(f)
+    o.setFrame(f)
+    _vray_for_blender.setFrame(f)
+
+    # 1. Export full first frame
+    err = exp_scene.ExportScene(bus)
+    if err is not None:
+        return err
+
+    # 2. Export nodes for rest frames
+    f += o.frameStep
+    while(f <= o.frameEnd):
+        scene.frame_set(f)
+        o.setFrame(f)
+        _vray_for_blender.setFrame(f)
+
+        err = exp_scene.ExportScene(bus, exportMeshes=False)
+        if err is not None:
+            break
+
+        f += o.frameStep
+
+    exp_init.ShutdownExporter(bus)
+
+    # Restore selected frame
+    scene.frame_set(selected_frame)
+
+    return None
+
+
+def ExportFullRange(bus):
+    scene = bus['scene']
+    o     = bus['output']
+
+    err = None
+
+    # Init exporter
+    bus['exporter'] = exp_init.InitExporter(bus, isAnimation=True)
+
+    # Store current frame
+    selected_frame = scene.frame_current
+
+    f = o.frameStart
+    while(f <= o.frameEnd):
         scene.frame_set(f)
 
         o.setFrame(f)
@@ -68,11 +140,36 @@ def ExportAnimation(bus, frameStart, frameEnd, frameStep):
         if err is not None:
             break
 
-        f += frameStep
+        f += o.frameStep
 
     # Restore selected frame
     scene.frame_set(selected_frame)
 
     exp_init.ShutdownExporter(bus)
+
+    return err
+
+
+@debug.TimeIt
+def ExportAnimation(bus, frameStart, frameEnd, frameStep):
+    err = None
+
+    scene = bus['scene']
+    o     = bus['output']
+
+    VRayScene = scene.vray
+    VRayExporter = VRayScene.Exporter
+
+    o.setAnimation(True)
+    o.setFrameStart(frameStart)
+    o.setFrameEnd(frameEnd)
+    o.setFrameStep(frameStep)
+
+    if VRayExporter.animation_type == 'NOTMESHES':
+        err = ExportFullNotMeshes(bus)
+    elif VRayExporter.animation_type == 'CAMERA':
+        err = ExportCameraOnly(bus)
+    else:
+        err = ExportFullRange(bus, frameStart, frameEnd, frameStep)
 
     return err
