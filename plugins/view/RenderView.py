aea45a9314837da2257d13c95e1960462b93dbb8
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-05-31 14:38:01 +0300

    add: new stereo settings

--- plugins/view/RenderView.py
+++ plugins/view/RenderView.py
@@ -129,6 +129,63 @@ PluginParams = (
         'type' : 'FLOAT',
         'default' : 1,
     },
+    {
+        'attr' : 'stereo_on',
+        'desc' : "Enable stereo rendering",
+        'type' : 'BOOL',
+        'default' : False,
+    },
+    {
+        'attr' : 'stereo_eye_distance',
+        'desc' : "The distance between the two stereo views",
+        'type' : 'FLOAT',
+        'default' : 0.065,
+    },
+    {
+        'attr' : 'stereo_interocular_method',
+        'desc' : "Specifies the camera position relative to the left and right views",
+        'type' : 'ENUM',
+        'items' : (
+            ('0', "Center",     ""),
+            ('1', "Keep Left",  ""),
+            ('2', "Keep Right", "")
+        ),
+        'default' : '0',
+    },
+    {
+        'attr' : 'stereo_specify_focus',
+        'desc' : "Specify a separate distance for the stereo focus",
+        'type' : 'BOOL',
+        'default' : False,
+    },
+    {
+        'attr' : 'stereo_focus_distance',
+        'desc' : "The focus distance",
+        'type' : 'FLOAT',
+        'default' : 1,
+    },
+    {
+        'attr' : 'stereo_focus_method',
+        'desc' : "The focus method",
+        'type' : 'ENUM',
+        'items' : (
+            ('0', "Parallel", "Both cameras have their focus points directly in front of them"),
+            ('1', "Rotation", "The stereoscopy is achieved by rotating the left and right views so that their focus points coincide at the distance from the eyes where the lines of sight for each eye converge called fusion distance"),
+            ('2', "Shear",    "The orientation of both views remain the same but each eyes view is sheared along Z so that the two frustums converge at the fusion distance"),
+        ),
+        'default' : '0',
+    },
+    {
+        'attr' : 'stereo_view',
+        'desc' : "Specifies which view to render",
+        'type' : 'ENUM',
+        'items' : (
+            ('0', "Both",  "Both views will be rendered side by side"),
+            ('1', "Left",  "Only the left view will be rendered"),
+            ('2', "Right", "Only the right view will be rendered")
+        ),
+        'default' : '0',
+    },
 
     {
         'attr' : 'clip_near',
@@ -159,9 +216,11 @@ def writeDatablock(bus, pluginModule, pluginName, propGroup, overrideParams):
         return
 
     VRayCamera = ca.data.vray
-    RenderView     = VRayCamera.RenderView
+    RenderView = VRayCamera.RenderView
     SettingsCamera = VRayCamera.SettingsCamera
     SettingsCameraDof = VRayCamera.SettingsCameraDof
+    CameraStereoscopic = VRayCamera.CameraStereoscopic
+    VRayStereoscopicSettings = VRayScene.VRayStereoscopicSettings
 
     fov, orthoWidth = BlenderUtils.GetCameraFOV(ca)
 
@@ -184,5 +243,22 @@ def writeDatablock(bus, pluginModule, pluginName, propGroup, overrideParams):
 
     overrideParams['focalDistance'] = BlenderUtils.GetCameraDofDistance(ca)
     overrideParams['aperture']      = SettingsCameraDof.aperture
+
+    if VRayStereoscopicSettings.use and not CameraStereoscopic.use:
+        overrideParams['stereo_on']                 = True
+        overrideParams['stereo_eye_distance']       = VRayStereoscopicSettings.eye_distance
+        overrideParams['stereo_interocular_method'] = VRayStereoscopicSettings.interocular_method
+        overrideParams['stereo_specify_focus']      = VRayStereoscopicSettings.specify_focus
+        overrideParams['stereo_focus_distance']     = VRayStereoscopicSettings.focus_distance
+        overrideParams['stereo_focus_method']       = VRayStereoscopicSettings.focus_method
+        overrideParams['stereo_view']               = VRayStereoscopicSettings.view
+    else:
+        overrideParams['stereo_on']                 = None
+        overrideParams['stereo_eye_distance']       = None
+        overrideParams['stereo_interocular_method'] = None
+        overrideParams['stereo_specify_focus']      = None
+        overrideParams['stereo_focus_distance']     = None
+        overrideParams['stereo_focus_method']       = None
+        overrideParams['stereo_view']               = None
 
     return ExportUtils.WritePluginCustom(bus, pluginModule, pluginName, propGroup, overrideParams)
