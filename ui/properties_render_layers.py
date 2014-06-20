ad79cb9693922265f5edcf00a8e0b29645eb0ffc
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-06-20 21:31:40 +0300

    fix: panels moved to render channels cause there is more space

--- ui/properties_render_layers.py
+++ ui/properties_render_layers.py
@@ -46,9 +46,115 @@ class VRayPanelRenderElements(classes.VRayRenderLayersPanel):
 		layout.operator("vray.convert_scene")
 
 
+class VRAY_SP_ntrees(classes.VRayScenePanel):
+	bl_label   = "Node Trees"
+	bl_options = {'DEFAULT_CLOSED'}
+
+	def draw(self, context):
+		VRayExporter = context.scene.vray.Exporter
+
+		self.layout.template_list("VRayListNodeTrees", "", bpy.data, 'node_groups', VRayExporter, 'ntreeListIndex', rows = 4)
+		self.layout.operator("vray.del_nodetree", text="Delete Selected Nodetree", icon="ZOOMOUT")
+
+		box = self.layout.box()
+		box.prop(VRayExporter, 'ntreeExportDirectory', text="Path")
+		box.separator()
+		box.operator("vray.export_nodetree", text="Export Selected Nodetree", icon='NODETREE')
+
+
+class VRAY_SP_lights_tweaker(classes.VRayScenePanel):
+	bl_label   = "Lights"
+	bl_options = {'DEFAULT_CLOSED'}
+
+	def draw(self, context):
+		wide_ui= context.region.width > classes.narrowui
+
+		layout= self.layout
+
+		split= layout.split()
+		col= split.column()
+
+		if bpy.data.lamps:
+			for lamp in bpy.data.lamps:
+				VRayLight = lamp.vray
+				VRayScene = context.scene.vray
+
+				lightPluginName = LibUtils.GetLightPluginName(lamp)
+
+				lightPropGroup = getattr(VRayLight, lightPluginName)
+
+				sub_t = col.row()
+				sub_t.label(text=" %s" % lamp.name, icon='LAMP_%s' % lamp.type)
+
+				if lightPluginName == 'SunLight':
+					sub = col.row()
+
+					sub.prop(lightPropGroup, 'enabled', text="")
+
+					r = sub.row()
+					r.scale_x = 0.4
+					r.prop(lightPropGroup, 'filter_color',   text="")
+
+					sub.prop(lightPropGroup, 'intensity_multiplier', text="")
+					sub.prop(lightPropGroup, 'shadow_subdivs',       text="")
+				else:
+					sub = col.row()
+					sub.prop(lightPropGroup, 'enabled', text="")
+					sub.prop(lightPropGroup, 'color',   text="")
+					sub.prop(lightPropGroup, 'intensity', text="")
+					sub.prop(lightPropGroup, 'subdivs',   text="")
+		else:
+			col.label(text="Nothing in bpy.data.lamps...")
+
+
+class VRAY_SP_includer(classes.VRayScenePanel):
+	bl_label   = "Include *.vrscene"
+	bl_options = {'DEFAULT_CLOSED'}
+
+	def draw_header(self, context):
+		VRayScene = context.scene.vray
+		Includer  = VRayScene.Includer
+		self.layout.prop(Includer, 'use', text="")
+
+	def draw(self, context):
+		wide_ui= context.region.width > classes.narrowui
+
+		layout= self.layout
+
+		row= layout.row()
+
+		vs= context.scene.vray
+		module= vs.Includer
+
+		layout.active= module.use
+
+		row.template_list("VRayListUse", "", module, 'nodes', module, 'nodes_selected', rows = 4)
+
+		col= row.column()
+		sub= col.row()
+		subsub= sub.column(align=True)
+		subsub.operator('vray.includer_add',    text="", icon="ZOOMIN")
+		subsub.operator('vray.includer_remove', text="", icon="ZOOMOUT")
+		sub= col.row()
+		subsub= sub.column(align=True)
+		subsub.operator("vray.includer_up",   icon='MOVE_UP_VEC',   text="")
+		subsub.operator("vray.includer_down", icon='MOVE_DOWN_VEC', text="")
+
+		if module.nodes_selected >= 0 and len(module.nodes) > 0:
+			render_node= module.nodes[module.nodes_selected]
+
+			layout.separator()
+
+			layout.prop(render_node, 'name')
+			layout.prop(render_node, 'scene')
+
+
 def GetRegClasses():
 	return (
 		VRayPanelRenderElements,
+		VRAY_SP_ntrees,
+		VRAY_SP_lights_tweaker,
+		VRAY_SP_includer,
 	)
 
 
