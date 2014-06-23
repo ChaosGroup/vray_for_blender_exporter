a1201a42a806ff58dcc6df513e399b4817746527
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-06-23 18:33:40 +0300

    fix: add group node items

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -86,6 +86,9 @@ def GetCategories():
             "Blender",
             items = [
                 nodeitems_utils.NodeItem("ShaderNodeNormal", label="Normal"),
+                nodeitems_utils.NodeItem("ShaderNodeGroup", label="Group"),
+                nodeitems_utils.NodeItem("NodeGroupInput", label="Group Input"),
+                nodeitems_utils.NodeItem("NodeGroupOutput", label="Group Output"),
             ],
             icon = 'BLENDER'
         ),
