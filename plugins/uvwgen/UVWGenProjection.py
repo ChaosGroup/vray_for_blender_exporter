7865635a3cc50651b8315d7bdd895bfd1536a79d
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-07-10 14:53:48 +0300

    fix: load desc from json

--- plugins/uvwgen/UVWGenProjection.py
+++ plugins/uvwgen/UVWGenProjection.py
@@ -22,12 +22,7 @@
 # All Rights Reserved. V-Ray(R) is a registered trademark of Chaos Software.
 #
 
-import math
-
-import bpy
-import mathutils
-
-from vb30.lib import ExportUtils
+from vb30.lib import PluginUtils
 
 
 TYPE = 'UVWGEN'
@@ -35,144 +30,10 @@ ID   = 'UVWGenProjection'
 NAME = 'Projection'
 DESC = "Projection mapping"
 
-PluginParams = (
-    {
-        'attr' : 'uvw_transform',
-        'desc' : "",
-        'type' : 'TRANSFORM',
-        'default' : "",
-    },
-    {
-        'attr' : 'uvw_transform_tex',
-        'desc' : "",
-        'type' : 'TRANSFORM_TEXTURE',
-        'default' : "",
-    },
-    {
-        'attr' : 'tex_transform',
-        'desc' : "",
-        'type' : 'TRANSFORM',
-        'default' : "",
-    },
-    {
-        'attr' : 'camera_settings',
-        'desc' : "",
-        'type' : 'PLUGIN',
-        'default' : "",
-    },
-    {
-        'attr' : 'camera_view',
-        'desc' : "",
-        'type' : 'PLUGIN',
-        'default' : "",
-    },
-    {
-        'attr' : 'bitmap',
-        'desc' : "If we are projecting a bitmap, this is the bitmap's buffer, we need it to implement vertical/horizontal fit",
-        'type' : 'PLUGIN',
-        'default' : "",
-    },
-    {
-        'attr' : 'type',
-        'desc' : "Mapping type",
-        'type' : 'ENUM',
-        'items' : (
-            ('0', "None", ""),
-            ('1', "Planar", ""),
-            ('2', "Spherical", ""),
-            ('3', "Cylindrical", ""),
-            ('4', "Ball", ""),
-            ('5', "Cubic", ""),
-            ('6', "Triplanar", ""),
-            ('8', "Perspective", ""),
-        ),
-        'default' : '1',
-    },
-    {
-        'attr' : 'fitfill',
-        'name' : 'Fit Fill',
-        'desc' : "Fit fill",
-        'type' : 'ENUM',
-        'items' : (
-            ('0', "Fit Fill", ""),
-            ('1', "Horizontal", ""),
-            ('2', "Vertical", ""),
-        ),
-        'default' : '0',
-    },
-    {
-        'attr' : 'fittype',
-        'name' : 'Fit Type',
-        'desc' : "Fit type",
-        'type' : 'ENUM',
-        'items' : (
-            ('0', "None", ""),
-            ('1', "Match camera film gate", ""),
-            ('2', "Match camera resolution", ""),
-        ),
-        'default' : '0',
-    },
-    {
-        'attr' : 'u_angle',
-        'desc' : "",
-        'type' : 'FLOAT',
-        'default' : 3.14159,
-    },
-    {
-        'attr' : 'v_angle',
-        'desc' : "",
-        'type' : 'FLOAT',
-        'default' : 1.5708,
-    },
-    {
-        'attr' : 'film_gate_w',
-        'desc' : "",
-        'type' : 'FLOAT',
-        'default' : 1,
-    },
-    {
-        'attr' : 'film_gate_h',
-        'desc' : "",
-        'type' : 'FLOAT',
-        'default' : 1,
-    },
-    {
-        'attr' : 'hide_backface',
-        'desc' : "Determine whether to project on back faces of polygons when in perspective mode",
-        'type' : 'BOOL',
-        'default' : False,
-    },
-    {
-        'attr' : 'hide_occluded',
-        'desc' : "Determine whether to project on occluded points when in perspective mode",
-        'type' : 'BOOL',
-        'default' : False,
-    },
-)
+PluginParams = PluginUtils.GetPluginParams(ID)
+PluginWidget = PluginUtils.GetPluginWidget(ID)
 
 
-def nodeDraw(context, layout, UVWGenProjection):
-    layout.prop(UVWGenProjection, 'type')
-
-
-def gui(context, layout, UVWGenProjection):
-    layout.prop(UVWGenProjection, 'type')
-    layout.prop(UVWGenProjection, 'fitfill')
-    layout.prop(UVWGenProjection, 'fittype')
-
-    layout.separator()
-
-    split = layout.split()
-    row = split.row(align=True)
-    row.prop(UVWGenProjection, 'u_angle')
-    row.prop(UVWGenProjection, 'v_angle')
-
-    split = layout.split()
-    row = split.row(align=True)
-    row.prop(UVWGenProjection, 'film_gate_w')
-    row.prop(UVWGenProjection, 'film_gate_h')
-
-    split = layout.split()
-    row = split.row()
-    row.prop(UVWGenProjection, 'hide_backface')
-    row.prop(UVWGenProjection, 'hide_occluded')
+def nodeDraw(context, layout, propGroup):
+    layout.prop(propGroup, 'type')
+    layout.prop(propGroup, 'object_space')
