18e9693cdc86af9824dbaa0e08aa3c1e359354a6
Andrei Izrantcev <andrei.izrantcev@chaosgroup.com>
2014-08-20 14:00:40 +0300

    fix: activate add node tree hotkey SHIFT+TAB

--- keymap.py
+++ keymap.py
@@ -37,8 +37,8 @@ def register():
         kmi = km.keymap_items.new('wm.call_menu_pie', 'TAB', 'PRESS')
         kmi.properties.name = "vray.show_ntree_pie"
 
-        # kmi = km.keymap_items.new('wm.call_menu_pie', 'TAB', 'PRESS', shift=True)
-        # kmi.properties.name = "vray.add_ntree_pie"
+        kmi = km.keymap_items.new('wm.call_menu_pie', 'TAB', 'PRESS', shift=True)
+        kmi.properties.name = "vray.add_ntree_pie"
 
         addon_keymaps.append(km)
 
