c7f71346bf469bf21460591d5ea5c1a1303749c2
Lyubomir <poseidon4o.random@gmail.com>
2016-03-02 18:14:06 +0200

    fix: do not set VRAY_ZMQSERVER_APPSDK_PATH before running zmq

--- engine.py
+++ engine.py
@@ -80,15 +80,7 @@ def _check_zmq_process(port):
         if not executable_path or not os.path.exists(executable_path):
             _debug("Can't find V-Ray ZMQ Server!")
         else:
-            if 'VRAY_ZMQSERVER_APPSDK_PATH' not in os.environ:
-                _debug("VRAY_ZMQSERVER_APPSDK_PATH is not set!")
-            else:
-                appsdkPath = os.environ['VRAY_ZMQSERVER_APPSDK_PATH']
-                os.environ['VRAY_PATH'] = os.path.dirname(appsdkPath)
-                if 'LD_LIBRARY_PATH' in os.environ:
-                    os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ':' + os.environ['VRAY_PATH']
-
-                _zmq_process = subprocess.Popen([executable_path, "-p", port])
+            _zmq_process = subprocess.Popen([executable_path, "-p", port])
 
 
 class VRayRenderer(bpy.types.RenderEngine):
