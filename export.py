7bdb8190bb9ede765084372ef6269a34bab59b07
Lyubomir Koev <lyubomir.koev@chaosgroup.com>
2017-02-14 16:55:07 +0200

    fix: vb35 std export correct animation

--- export.py
+++ export.py
@@ -149,6 +149,12 @@ def ExportEx(bus):
         if not rtExporter:
             err = Export(bus, scene, engine, engine.is_preview)
         else:
+            if not VRayExporter.animation_mode in {'NONE', 'CAMERA_LOOP'}:
+                o.setAnimation(True)
+                o.setFrameStart(scene.frame_start)
+                o.setFrameEnd(scene.frame_end)
+                o.setFrameStep(scene.frame_step)
+
             init = {
                 'context'      : bpy.context.as_pointer(),
                 'engine'       : engine.as_pointer(),
@@ -176,7 +182,7 @@ def ExportEx(bus):
 
                 setattr(engine, 'renderer', renderer)
 
-                _vray_for_blender_rt.update(renderer)
+                _vray_for_blender_rt.render(renderer)
 
     except Exception as e:
         debug.ExceptionInfo(e)
